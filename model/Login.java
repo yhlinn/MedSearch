ef1de875ec94ddcb99241a898c144f70ef45e95e
yhlinn <54613391+yhlinn@users.noreply.github.com>
2021-09-06 21:43:23 -0700

    models

---
+++ model/Login.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Login {
+	protected String username;
+	protected String password;
+	
+	public Login(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
